d038ed8f4ac69fa70b0a4285356ff7df4a07b995
martijn van der ploeg <martijn.ploeg@gmail.com>
2021-01-01 11:00:40 +0100

    lab1

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/martijnxd/testwebservice/models"
+)
 
 func main() {
-	fmt.Println("Test from module")
+	u := models.User{
+		ID:        2,
+		FirstName: "Martijn",
+		LastName:  "Last name",
+	}
+	fmt.Println(u)
 }
