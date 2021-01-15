2013b19ebdc65ddbe76cf8a3b48b3ff568cd5112
martijnvdp <m.vanderploeg@nl.ccv.eu>
2021-01-15 20:39:48 +0100

    testing with go test

---
+++ main_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	cases := []struct {
+		in, out string
+	}{
+		{"martijn@golang.org", "Hello gopher martijn"},
+		{"something", "hello something"},
+	}
+	for _, c := range cases {
+		req, err := http.NewRequest(
+			http.MethodGet,
+			"http://localhost:8080/"+c.in,
+			nil,
+		)
+		if err != nil {
+			t.Fatalf("could not create request: %v", err)
+		}
+		rec := httptest.NewRecorder()
+		handler(rec, req)
+
+		if rec.Code != http.StatusOK {
+			t.Errorf("expected status 200;got %d", rec.Code)
+		}
+		if !strings.Contains(rec.Body.String(), c.out) {
+			t.Errorf("unexpected body in respose: %q", rec.Body.String())
+		}
+	}
+}
+
+func BenchmarkHandler(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		req, err := http.NewRequest(
+			http.MethodGet,
+			"http://localhost:8080/martijn@golang.org",
+			nil,
+		)
+		if err != nil {
+			b.Fatalf("could not create request: %v", err)
+		}
+		rec := httptest.NewRecorder()
+		handler(rec, req)
+
+		if rec.Code != http.StatusOK {
+			b.Errorf("expected status 200;got %d", rec.Code)
+		}
+		if !strings.Contains(rec.Body.String(), "gopher martijn") {
+			b.Errorf("unexpected body in respose: %q", rec.Body.String())
+		}
+	}
+}
